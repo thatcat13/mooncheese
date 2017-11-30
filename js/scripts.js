e6e324a6f4166b95333817cb3cb8b9775ac3b08d
Cat Janowitz and Shane Ryan <cat@>
2017-11-30 15:58:15 -0800

    add video the old fashioned way after trying a fancy plugin and not working

--- js/scripts.js
+++ js/scripts.js
@@ -2,5 +2,20 @@ $(document).ready(function(event){
   event.preventDefault;
   $('.menu-btn' ).click(function() {
     $('.responsive-menu').toggleClass('expand')
+
   });
+  // $('#my-video-display').prettyEmbed({
+  // 	videoID: 'T0qagA4_eVQ',
+  // 	previewSize: 'hd',				// use either this option...
+  //
+  // 	// Embed controls
+  // 	showInfo: false,
+  // 	showControls: true,
+  // 	loop: true,
+  //
+  // 	colorScheme: 'dark',
+  // 	showRelated: false,
+  //
+  // 	useFitVids: true
+  // });
 });
