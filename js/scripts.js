e68f70ba23d704e93717a92c16c5834020612566
Cat Janowitz and Shane Ryan <cat@>
2017-11-30 16:23:35 -0800

    toggle menu off when screen is resized

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,10 @@
-$(document).ready(function(event){
-  event.preventDefault;
+$(document).ready(function(){
   $('.menu-btn' ).click(function() {
-    $('.responsive-menu').toggleClass('expand')
-
+    $('.responsive-menu').toggleClass('expand');
   });
-  // $('#my-video-display').prettyEmbed({
-  // 	videoID: 'T0qagA4_eVQ',
-  // 	previewSize: 'hd',				// use either this option...
-  //
-  // 	// Embed controls
-  // 	showInfo: false,
-  // 	showControls: true,
-  // 	loop: true,
-  //
-  // 	colorScheme: 'dark',
-  // 	showRelated: false,
-  //
-  // 	useFitVids: true
-  // });
+  $(window).resize(function(){
+    if ($(window).width() > 980){
+      $('.responsive-menu').removeClass('expand');
+    }
+  });
 });
